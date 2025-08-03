85d157f8eaec1038d0475c3a5e0cf7a8928edf95
Catan <admin@catanix.dev>
2025-08-03 21:43:54 +0500

    fix(tasks): fixed voice event handler

--- src/bot/handlers/actions/init.js
+++ src/bot/handlers/actions/init.js
@@ -1,10 +1,11 @@
-import { setupConfirmHandler } from "../voiceHandler.js";
+import { setupConfirmHandler, voiceHandler } from "../voiceHandler.js";
 import { registerMusicActions } from "../commands/musicSearchHandler.js";
 import { textHandler } from "../textHandler.js";
 import { registerYoutubeDownloadAction } from "./youtubeDownloadAction.js";
 
 export const initBotHandlersActions = (bot) => {
     bot.on('text', textHandler);
+    bot.on('voice', voiceHandler);
     setupConfirmHandler(bot);
     registerMusicActions(bot);
     registerYoutubeDownloadAction(bot);
